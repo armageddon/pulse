0493bfcac02c571498136dbbfa55b516bc14d34f
armageddon <pierredane@hotmail.com>
2009-09-16 15:48:26 +0100

    fixed read mail

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090916132702) do
+ActiveRecord::Schema.define(:version => 20090916135020) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -62,30 +62,31 @@ ActiveRecord::Schema.define(:version => 20090916132702) do
   end
 
   create_table "ldc", :force => true do |t|
-    t.string "LDC_ID",             :limit => 512
-    t.string "b_NameFull",         :limit => 512
-    t.string "ad_Building_Unit",   :limit => 512
-    t.string "ad_Building",        :limit => 512
-    t.string "ad_Building_careof", :limit => 512
-    t.string "ad_StreetNumber",    :limit => 512
-    t.string "ad_Street",          :limit => 512
-    t.string "ad_Zip",             :limit => 512
-    t.string "area_name",          :limit => 512
-    t.string "ad_City",            :limit => 512
-    t.string "ad_county",          :limit => 512
-    t.string "bus_telephone",      :limit => 512
-    t.string "website_name",       :limit => 512
-    t.string "b_multipleID",       :limit => 512
-    t.string "b_multipleName",     :limit => 512
-    t.string "cat_ID1",            :limit => 512
-    t.string "cat_Name1",          :limit => 512
-    t.string "cat_ID2",            :limit => 512
-    t.string "cat_Name2",          :limit => 512
-    t.string "transport_name",     :limit => 512
-    t.string "transport_carrier",  :limit => 512
-    t.text   "export_description"
-    t.string "feature_names",      :limit => 512
-    t.string "feature_price",      :limit => 512
+    t.string  "LDC_ID",             :limit => 512
+    t.string  "b_NameFull",         :limit => 512
+    t.string  "ad_Building_Unit",   :limit => 512
+    t.string  "ad_Building",        :limit => 512
+    t.string  "ad_Building_careof", :limit => 512
+    t.string  "ad_StreetNumber",    :limit => 512
+    t.string  "ad_Street",          :limit => 512
+    t.string  "ad_Zip",             :limit => 512
+    t.string  "area_name",          :limit => 512
+    t.string  "ad_City",            :limit => 512
+    t.string  "ad_county",          :limit => 512
+    t.string  "bus_telephone",      :limit => 512
+    t.string  "website_name",       :limit => 512
+    t.string  "b_multipleID",       :limit => 512
+    t.string  "b_multipleName",     :limit => 512
+    t.string  "cat_ID1",            :limit => 512
+    t.string  "cat_Name1",          :limit => 512
+    t.string  "cat_ID2",            :limit => 512
+    t.string  "cat_Name2",          :limit => 512
+    t.string  "transport_name",     :limit => 512
+    t.string  "transport_carrier",  :limit => 512
+    t.text    "export_description"
+    t.string  "feature_names",      :limit => 512
+    t.string  "feature_price",      :limit => 512
+    t.boolean "exclude",                           :default => false
   end
 
   add_index "ldc", ["id"], :name => "idx_id"
