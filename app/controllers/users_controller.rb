444034402ce6dec42b16709345d8eeea8866dd1b
armageddon <pierredane@hotmail.com>
2009-09-17 13:44:10 +0100

    image upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @places = current_user.suggested_places
     @matches = current_user.matches(params[:page], 8)
   end
+  end
 
   def new
     logger.debug("In Create")
@@ -67,21 +68,24 @@ class UsersController < ApplicationController
   end
 
   def update
-    if params[:iframe]!="true"
+    if params[:iframe]=="true"
+      
+    else
        params[:user][:dob] = Date.new(params[:year].to_i(),params[:month].to_i(),params[:day].to_i())
        logger.debug(params[:user][:dob])
        logger.debug(current_user.get_age_option_from_dob(params[:user][:dob]))
        params[:user][:age] = current_user.get_age_option_from_dob(params[:user][:dob])
-    
     end
-    logger.debug("in update")    
+   
     respond_to do |format|
-      if params[:iframe] == true
-        logger.debug("in iframe") 
-        format.html {render :text => "sdfsdfsdf"}
-      elsif current_user.update_attributes(params[:user])
-        format.js { render current_user.icon}
-        format.html { redirect_to account_path }
+      if current_user.update_attributes(params[:user])
+        if params[:iframe] == "true"
+          format.js { render :text => "dsdsdsd"}
+          format.html { render :text => "qaaaaaaa" }
+        else
+          format.js { render current_user.icon}
+          format.html { redirect_to account_path }
+        end
       else
         format.js { render :nothing => true, :status => 500 }
         format.html { render :action => :new }
