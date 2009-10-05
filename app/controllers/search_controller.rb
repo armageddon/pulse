1fa7d047edc71b793d40fe6977db3553b1415604
armageddon <pierredane@hotmail.com>
2009-10-05 17:38:29 +0100

    fixed find places

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -44,7 +44,7 @@ class SearchController < ApplicationController
           high_long = longitude + long_range
           logger.debug("before search")
           @place_ids =  Place.all(:select => "DISTINCT place_id" , :conditions => ["latitude <= ? and latitude >= ? and longitude >= ? and longitude <= ?",high_lat,low_lat,low_long,high_long])
-          @results = UserActivity.paginate(:select => "DISTINCT place_id, activity_id ",:conditions => {:place_id => @place_ids, :activity_id => params[:activity_id]}, :include => [:place, :activity],:page => params[:page], :per_page => 12)
+          @results = UserActivity.paginate(:select => "DISTINCT id, activity_id ",:conditions => {:place_id => @place_ids, :activity_id => params[:activity_id]}, :include => [:place, :activity],:page => params[:page], :per_page => 12)
         end
       end
 
