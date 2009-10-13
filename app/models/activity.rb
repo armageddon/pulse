9cc881cce579fbd0eaaa0e0bb74fd4bce4e2dc87
armageddon <pierredane@hotmail.com>
2009-10-13 13:15:42 +0100

    changed case of questionmark image call

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,7 @@ class Activity < ActiveRecord::Base
   define_index do
     indexes name
   end
-  has_attached_file :icon, :styles => { :thumb => "75x75#", :thumb => "160x160#" }, :default_url => "/images/question.png"
+  has_attached_file :icon, :styles => { :thumb => "75x75#", :thumb => "160x160#" }, :default_url => "/images/Question.png"
   has_many :user_activities
   has_many :user_place_activities
   has_many :users, :through => :user_activities
