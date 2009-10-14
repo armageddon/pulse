652683f138aeffea78847bb8724fa0fb9ab38398
armageddon <pierredane@hotmail.com>
2009-10-14 14:32:08 +0100

    removed description validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,8 +73,8 @@ class User < ActiveRecord::Base
   validates_length_of       :email,       :within => 6..100 #r@a.wk
   validates_format_of       :email,       :with => Authentication.email_regex, :message => Authentication.bad_email_message
   
-  validates_presence_of     :description
-  validates_length_of       :description,  :maximum => 250
+ # validates_presence_of     :description
+ # validates_length_of       :description,  :maximum => 250
   
   validates_inclusion_of    :sex_preference, :in => [Sex::MALE, Sex::FEMALE, Sex::BOTH], :allow_blank => true
   validates_inclusion_of    :sex, :in => [Sex::MALE, Sex::FEMALE], :allow_blank => true
