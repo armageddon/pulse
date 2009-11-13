447e6ad9e219db8f7c4481d6b62cd680abc2d9ce
armageddon <pierredane@hotmail.com>
2009-11-13 16:07:24 +0000

    Paging

--- app/helpers/css_helper.rb
+++ app/helpers/css_helper.rb
@@ -33,8 +33,40 @@ module CssHelper
       when "2" then ''
       when "3" then 'criteria_highlight'
       when "4" then 'criteria_highlight'
+      when "5" then 'criteria_highlight'
       else ''
     end
   end
   
+  
+  def search_type_simple_class(search_type)
+    case search_type
+      when "1" then 'search_type_selected'
+      when "2" then 'search_type_selected'
+      when "3" then 'search_type_selected'
+      when "4" then 'search_type'
+      when "5" then 'search_type'
+      else ''
+    end
+  end
+  def search_type_advanced_class(search_type)
+    case search_type
+      when "1" then 'search_type'
+      when "2" then 'search_type'
+      when "3" then 'search_type'
+      when "4" then 'search_type_selected'
+      when "5" then 'search_type'
+      else ''
+    end
+  end
+  def search_type_map_class(search_type)
+    case search_type
+      when "1" then 'search_type'
+      when "2" then 'search_type'
+      when "3" then 'search_type'
+      when "4" then 'search_type'
+      when "5" then 'search_type_selected'
+      else ''
+    end
+  end
 end
