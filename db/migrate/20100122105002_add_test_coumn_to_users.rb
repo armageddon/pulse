904118adcc6506f50933f8807b6fbb9fc2a30d21
armageddon <pierredane@hotmail.com>
2010-01-22 10:50:40 +0000

    test

---
+++ db/migrate/20100122105002_add_test_coumn_to_users.rb
@@ -0,0 +1,10 @@
+class AddTestCoumnToUsers < ActiveRecord::Migration
+  def self.up
+     add_column :users , :c, :integer, :default => 1
+   end
+
+   def self.down
+      remove_column :c , :status
+
+   end
+end
