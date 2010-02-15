cc9255af2a7604b4e52361a2971d785b6f407397
Al <apdavidson@gmail.com>
2010-02-15 17:51:39 +0000

    added gems

--- config/environment.rb
+++ config/environment.rb
@@ -17,6 +17,9 @@ Rails::Initializer.run do |config|
   config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com'
   config.gem 'faker'
   config.gem "right_aws"
+  
+  # added by Al
+  config.gem "rmagick"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
