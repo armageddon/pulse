7bf9765fcf74798e2de659b3c802843f433dba2f
armageddon <pierredane@hotmail.com>
2010-02-16 11:02:35 +0000

    removed rmagik

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   config.gem "right_aws"
   
   # added by Al
-  config.gem "rmagick"
+  #config.gem "rmagick"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
