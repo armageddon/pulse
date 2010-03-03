1cbf26c004f9c46987034c445b7a9e73c0632ab4
armageddon <pierredane@hotmail.com>
2010-03-03 15:37:29 +0000

    changed to use prod api key

--- app/models/user_place_activity.rb
+++ app/models/user_place_activity.rb
@@ -24,13 +24,13 @@ class UserPlaceActivity < ActiveRecord::Base
 
       #todo : lazy load this
     pulse_fb_session = Facebooker::Session.create
-    pulse_fb_session.auth_token = "ZMZ8SM"
+    pulse_fb_session.auth_token = "NH3XTZ" #ZMZ8SM"
 
     pulse_fb_session.post("facebook.stream.publish", :action_links=> '[{ "text": "Check out HelloPulse!", "href": "http://www.hellopulse.com"}]', :message => message, :uid=>279928867967)
 
     
 
-    rescue
+    rescue 
       logger.error('Ping failed')
     end
   end
