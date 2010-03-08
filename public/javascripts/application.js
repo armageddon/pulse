651ed8898251f10764d533239f7b13e8adeda61f
armageddon <pierredane@hotmail.com>
2010-03-08 18:49:29 +0000

    stage test 2

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $.ajaxSetup({ 
         'beforeSend': function(xhr) {
             xhr.setRequestHeader("Accept", "text/javascript")
-            } 
+        }
     });
   
     if ($.jqm) {
@@ -15,14 +15,17 @@ $(document).ready(function() {
     }
 
     $('#crop').live('click',function() {
-	   	$('#dialog').jqm({ajax:'/icon_crop',modal:true}); 
-		$('#dialog').jqmShow();
-	return false;
-	});
+        $('#dialog').jqm({
+            ajax:'/icon_crop',
+            modal:true
+        });
+        $('#dialog').jqmShow();
+        return false;
+    });
 	
-	$('#cropp').live('click',function() {
-		alert('crop');
-		$("#photo_crop").ajaxSubmit({
+    $('#cropp').live('click',function() {
+        alert('crop');
+        $("#photo_crop").ajaxSubmit({
             iframe: true,
             extraData: {
                 'iframe': 'true'
@@ -32,8 +35,8 @@ $(document).ready(function() {
                 alert('success');
             }
         })
-		return false;
-	});
+        return false;
+    });
 	
     $(document).ajaxSend(function(event, request, settings) {
         if (typeof(AUTH_TOKEN) == "undefined") return;
@@ -48,7 +51,7 @@ $(document).ready(function() {
             url: "/account/favorites/delete",
             data: {
                 "friend_id" : $(this).attr("friend_id")
-                },
+            },
             success: function(p) {
                 link.parent().parent().removeClass('object');
                 link.parent().parent().html('');
@@ -71,7 +74,7 @@ $(document).ready(function() {
             url: "/account/favorites",
             data: {
                 "friend_id" : $(this).attr("friend_id")
-                },
+            },
             success: function(p) {
                 link.removeClass("people_favorite");
                 link.addClass("people_unfavorite");
@@ -84,18 +87,18 @@ $(document).ready(function() {
         return false;
     })
     
-	$(".place_activity_people_favorite").live('click',function() {
+    $(".place_activity_people_favorite").live('click',function() {
         var link = $(this);
         $.ajax({
             type: "POST",
             url: "/account/favorites",
             data: {
                 "friend_id" : $(this).attr("friend_id")
-                },
+            },
             success: function(p) {
                 link.css("display","none");
               
-                  },
+            },
             error: function(p) {
                 alert("error");
             }
@@ -103,14 +106,14 @@ $(document).ready(function() {
         return false;
     })
     
-	$(".profile_people_unfavorite").live('click',function() {
+    $(".profile_people_unfavorite").live('click',function() {
         var link = $(this);
         $.ajax({
             type: "DELETE",
             url: "/account/favorites/delete",
             data: {
                 "friend_id" : $(this).attr("friend_id")
-                },
+            },
             success: function(p) {
                 $("#profile_people_unfavorite").css('display', 'none');
                 $("#profile_people_favorite").css('display', 'block');
@@ -122,18 +125,18 @@ $(document).ready(function() {
         return false;
     })
     
-	$(".profile_people_favorite").live('click',function() {
+    $(".profile_people_favorite").live('click',function() {
         var link = $(this);
         $.ajax({
             type: "POST",
             url: "/account/favorites",
             data: {
                 "friend_id" : $(this).attr("friend_id")
-                },
+            },
             success: function(p) {
-             	$("#profile_people_unfavorite").css('display', 'block');
+                $("#profile_people_unfavorite").css('display', 'block');
                 $("#profile_people_favorite").css('display', 'none');
-			},
+            },
             error: function(p) {
                 alert("error");
             }
@@ -149,13 +152,13 @@ $(document).ready(function() {
             url: "/user_place_activities/delete",
             data: {
                 "user_place_activity_id" : $(this).attr("user_place_activity_id")
-                },
+            },
             success: function(p) {
                 //need to determinw which page this comes from and remove if favorites
 
    
-                    link.parent().parent().removeClass('object');
-                    link.parent().parent().replaceWith('');
+                link.parent().parent().removeClass('object');
+                link.parent().parent().replaceWith('');
                 
                 
             },
@@ -187,7 +190,7 @@ $(document).ready(function() {
             $('#people_results').html(data);
             //$('#results_header').html(data.total_entries)
 
-			$('#people_paging').html('Page ' + $('#user_page').val() + ' of ' + $('#user_page_count').val() );
+            $('#people_paging').html('Page ' + $('#user_page').val() + ' of ' + $('#user_page_count').val() );
             $("#dialog").jqmAddTrigger('.add_to_favorites, .add_event, .add_activity');
         })
         return false;
@@ -198,12 +201,14 @@ $(document).ready(function() {
         $(this).children('.fancy_select_options').show();
     });
 
-	$('.std_dd').live('change',function() {
-                alert('selected');
-		$(this).prev().val($(this).selected().val());
-                  $(this).blur();
-                $(this).focus();
-	});
+    $('.std_dd').live('change',function() {
+        var cntrl = $(this);
+        alert(cntrl.selected().val());
+        cntrl.prev().val(cntrl.selected().val());
+        alert(cntrl.prev().val());
+        cntrl.blur();
+        cntrl.focus();
+    });
 
     $('.fancy_select_options p').live('mouseover',function() {
         $(this).addClass('hover');
@@ -216,21 +221,23 @@ $(document).ready(function() {
         return false;
     });
 
-	$('#activity_category_select').live('click',function() {
-	$.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "/search/activity_list",
-        data: { "activity_category_id" : $('#activity_category_id').val() },
-        success: function(p) {
-	 		var options = "";
-			for(i=0;i<p.length;i++)
-	        {
-				options += '<option value=' + p[i].activity.id + '>'+p[i].activity.name + '</option>';
-	 		}
-			$('#activity_select').html(options);
-			$('#activity_id').val(p[0].activity.id);
-        }
-      })
-	});
-  });
+    $('#activity_category_select').live('click',function() {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "/search/activity_list",
+            data: {
+                "activity_category_id" : $('#activity_category_id').val()
+            },
+            success: function(p) {
+                var options = "";
+                for(i=0;i<p.length;i++)
+                {
+                    options += '<option value=' + p[i].activity.id + '>'+p[i].activity.name + '</option>';
+                }
+                $('#activity_select').html(options);
+                $('#activity_id').val(p[0].activity.id);
+            }
+        })
+    });
+});
