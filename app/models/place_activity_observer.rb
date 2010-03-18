3940ea718aaa4d956e672bd28d57ff8e206d8359
armageddon <pierredane@hotmail.com>
2010-03-18 12:21:54 +0000

    place save

--- app/models/place_activity_observer.rb
+++ app/models/place_activity_observer.rb
@@ -8,12 +8,18 @@ class PlaceActivityObserver < ActiveRecord::Observer
   def after_save(place_activity)
     logger.debug(place_activity.place_id)
     logger.debug(place_activity.id)
-    if place_activity.url == nil && place_activity.id != 0  && place_activity.id != nil
-      bitly = Bitly.new('hellopulse','R_84b8e1abe448b0ef0517cea2e1a5eb42')
-      long_url = 'http://www.hellopulse.com/place_activities/'+place_activity.id.to_s
-      logger.debug(long_url )
-      page_url = bitly.shorten(long_url)
-      place_activity.url = page_url.shorten
+    begin
+      if place_activity.url == nil && place_activity.id != 0  && place_activity.id != nil
+        bitly = Bitly.new('hellopulse','R_84b8e1abe448b0ef0517cea2e1a5eb42')
+        long_url = 'http://www.hellopulse.com/place_activities/'+place_activity.id.to_s
+        logger.debug(long_url )
+        page_url = bitly.shorten(long_url)
+        place_activity.url = page_url.shorten
+        place_activity.save
+      end
+    rescue
+      logger.error('Place activity URL save error')
+      place_activity.url = 'www.bit.ly/atCD0U'
       place_activity.save
     end
   end
