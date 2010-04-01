b89b22a4c936f996a2eced4f7d716f5f9f2a2a0b
armageddon <pierredane@hotmail.com>
2010-04-01 15:25:24 +0100

    ping

--- app/models/user_place_activity_observer.rb
+++ app/models/user_place_activity_observer.rb
@@ -7,28 +7,36 @@ class UserPlaceActivityObserver < ActiveRecord::Observer
 
   def after_save(user_place_activity)
     if(user_place_activity.errors.length==0)
-      begin
-     
-        #check if activity or place has a partner
-        if user_place_activity.place.admin_user_id != nil
-          url = user_place_activity.place.url||'http://www.hellopulse.com/places/'+self.place.id.to_s
-        elsif user_place_activity.activity.admin_user_id != nil
-          url = user_place_activity.activity.url||'http://www.hellopulse.com/activities/'+self.activity.id.to_s
-        else
-          url = user_place_activity.place.url||'http://www.hellopulse.com/places/'+self.place.id.to_s
-        end
+ 
+      logger.debug('start')
+      #check if activity or place has a partner
+      if user_place_activity.place.admin_user_id != nil
+        url = user_place_activity.place.url||'http://www.hellopulse.com/places/'+user_place_activity.place.id.to_s
+      elsif user_place_activity.activity.admin_user_id != nil
+        url = user_place_activity.activity.url||'http://www.hellopulse.com/activities/'+user_place_activity.activity.id.to_s
+      else
+        url = user_place_activity.place.url||'http://www.hellopulse.com/places/'+user_place_activity.place.id.to_s
+      end
 
-        message = User.find(user_place_activity.user_id).first_name + ' added ' + Activity.find(user_place_activity.activity_id).name + ' at ' +Place.find(user_place_activity.place_id).name+ ' : ' + user_place_activity.description
-        tweet =  message[0,112]+'... '+ url
+      message = User.find(user_place_activity.user_id).first_name + ' added ' + Activity.find(user_place_activity.activity_id).name + ' at ' +Place.find(user_place_activity.place_id).name+ ' : ' + user_place_activity.description
+      tweet =  message[0,112]+'... '+ url
+      begin
         PingFM.user_post("status", tweet)
-        #todo : lazy load this
-        logger.info('message: ' + message +  'URL: ' + url)
-        pulse_fb_session = Facebooker::Session.create
-        pulse_fb_session.auth_token = "NH3XTZ" #ZMZ8SM"
+      rescue
+        logger.error('tweet failed')
+      ends
+      #todo : lazy load this
+      logger.info('message: ' + message +  'URL: ' + url)
+      pulse_fb_session = Facebooker::Session.create
+      pulse_fb_session.auth_token = "NH3XTZ" #ZMZ8SM"
+      begin
         pulse_fb_session.post("facebook.stream.publish", :action_links=> '[{ "text": "Check out HelloPulse!", "href": "'+url+'"}]', :message => message,  :uid=>279928867967)
       rescue
-        logger.error('Ping failed')
+        logger.error('fb failed')
       end
+
+      
+
     end
   end
 end
