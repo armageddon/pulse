b1ce245e10e91c10f4952235e9d91a1691572b2a
armageddon <pierredane@hotmail.com>
2010-04-05 08:48:18 +0100

    mail headers friendly name

--- app/models/invitation_mailer.rb
+++ app/models/invitation_mailer.rb
@@ -1,8 +1,10 @@
 class InvitationMailer < ActionMailer::Base
 
   def invitation(invitation)
-    @from    = "\"HelloPulse\"<admin@hellopulse.com>"
+    @from    = "\"HelloPulse\" <admin@hellopulse.com>"
     @subject = "You've been invited to HelloPulse"
+    headers["return-path"] = 'admin@hellopulse.com'
+
     @recipients = invitation.email
     @body[:url]  = "www.hellopulse.com"
     @body[:user] = invitation.user
