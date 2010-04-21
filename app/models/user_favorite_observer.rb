98281d9cd7c14d029f354f7b77129f867ecdc5c3
armageddon <pierredane@hotmail.com>
2010-04-21 11:43:17 +0100

    debugging# Please enter the commit message for your changes. Lines starting

--- app/models/user_favorite_observer.rb
+++ app/models/user_favorite_observer.rb
@@ -1,8 +1,9 @@
 class UserFavoriteObserver < ActiveRecord::Observer
   def after_create(user_favorite)
     friend = User.find(user_favorite.friend_id)
-      RAILS_DEFAULT_LOGGER.debug(user_favorite.friend_id.to_s + 'dffffffffffffffffffffffffffffff')
-       RAILS_DEFAULT_LOGGER.debug(friend.username)
+      RAILS_DEFAULT_LOGGER.info(user_favorite.friend_id.to_s + 'dffffffffffffffffffffffffffffff')
+       RAILS_DEFAULT_LOGGER.info(friend.username)
+       RAILS_DEFAULT_LOGGER.info(user_favorite.user.first_name)
     UserMailer.deliver_notification(user_favorite.user, friend) if user_favorite.user.status == 1 && friend.status==1
     
   end
