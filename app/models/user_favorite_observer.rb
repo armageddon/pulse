1282bca3cb6af6a81b3043d68067afb6fa1b8323
armageddon <pierredane@hotmail.com>
2010-04-21 12:09:54 +0100

    fav4

--- app/models/user_favorite_observer.rb
+++ app/models/user_favorite_observer.rb
@@ -4,7 +4,7 @@ class UserFavoriteObserver < ActiveRecord::Observer
       RAILS_DEFAULT_LOGGER.info(user_favorite.friend_id.to_s + 'dffffffffffffffffffffffffffffff')
        RAILS_DEFAULT_LOGGER.info(friend.username)
        RAILS_DEFAULT_LOGGER.info(user_favorite.user.first_name)
-    UserMailer.deliver_notification(user_favorite.user, friend) if user_favorite.user.status == 1 && friend.status==1
+    UserMailer.deliver_notification(friend , user_favorite.user) if user_favorite.user.status == 1 && friend.status==1
     
   end
 
