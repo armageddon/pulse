a6253ee76d6a0af66488956265fcb73e46a1fed4
armageddon <pierredane@hotmail.com>
2010-04-22 17:31:52 +0100

    removed date from feed

--- app/models/user_favorite_observer.rb
+++ app/models/user_favorite_observer.rb
@@ -2,7 +2,8 @@ class UserFavoriteObserver < ActiveRecord::Observer
   def after_create(user_favorite)
     begin
     friend = User.find(user_favorite.friend_id)
-    UserMailer.deliver_notification(friend , user_favorite.user) if user_favorite.user.status == 1 && friend.status==1
+
+    UserMailer.deliver_notification(friend , user_favorite.user) if user_favorite.user.status == 1 && friend.status==1 &&friend.note_messages=1
     rescue
       RAILS_DEFAULT_LOGGER.error('mailer error')
     end
