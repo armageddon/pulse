2ec1f712714c31c90c7f64dd5392b7c08a68d780
armageddon <pierredane@hotmail.com>
2010-06-14 09:50:25 +0100

    sd

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -8,22 +8,22 @@ class FacebookController < ApplicationController
   end
 
   def callback
-    logger.debug('XXXXXXXXXXXXXXXXXXXXXXXXX')
+    logger.info('XXXXXXXXXXXXXXXXXXXXXXXXX')
     #need to decide where to redirect to
     #if user  does not exist then go to link with dest so can redirect to original page.
     #if user exists then go to page button was pressedon
    
     if params[:code].present?
-      logger.debug('fb_test')
+      logger.info('fb_test')
       p params['code']
       access_token_hash = MiniFB.oauth_access_token(FB_APP_ID, CALLBACK_URL, FB_SECRET_KEY, params[:code])
-      logger.debug('XXXXXXXX' + access_token_hash["access_token"])
+      logger.info('XXXXXXXX' + access_token_hash["access_token"])
       @access_token = access_token_hash["access_token"]
-      logger.debug(@access_token)
+      logger.info(@access_token)
       cookies[:access_token] = @access_token
     end
     @user = MiniFB.get(cookies[:access_token], 'me')
-    logger.debug('USERID:' +@user.id )
+    logger.info('USERID:' +@user.id )
     user = User.find_by_fb_user_id(@user.id)
     if user != nil
     self.current_user = User.find_by_fb_user_id(@user.id)
